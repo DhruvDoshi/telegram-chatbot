0cfcd6c59a6bd3de92b6f2ee9de6987d158763a3
DhruvDoshi <dhruvdoshi25071999@gmail.com>
2020-05-17 09:13:34 +0530

    parsing the application code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+const axios = require('axios')
 
-
+app.use(bodyParser.json()) // for parsing application/json
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+) //for parsing application / x-www-form-urlencoded
